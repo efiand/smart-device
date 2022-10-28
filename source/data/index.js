4f23a46c5b08058e71c7359238214d5b67d1dcac
Andrey Efimov <efiand@ya.ru>
2022-10-28 15:36:03 +0300

    Разметка шапки

--- source/data/index.js
+++ source/data/index.js
@@ -1,1 +1,29 @@
-export default {};
+const [mainPhone, ...additionalPhones] = [
+  '+7 (495) 000 52 52',
+  '+7 (495) 589-40-50',
+  '+7 (925) 389-58-29',
+  '+7 (925) 364-78-13'
+].map((phone) => ({
+  phone,
+  shortPhone: `+${phone.replace(/\D/g, '')}`
+}));
+
+export default {
+  additionalPhones,
+  callBack: {
+    title: 'Заказать звонок',
+    url: '#!'
+  },
+  contacts: [
+    {
+      id: 'mailto',
+      value: 'info@sd.ru'
+    },
+    {
+      id: 'tel',
+      title: mainPhone.phone,
+      value: mainPhone.shortPhone
+    }
+  ],
+  slogan: 'умные технологии'
+};
