61f728a94e08576816677e915003d779ba9e3c30
Andrey Efimov <efiand@ya.ru>
2022-11-03 15:19:12 +0300

    Typograf

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,7 +1,20 @@
-const { getPosthtmlW3c } = require('pineglade-w3c');
+const {getPosthtmlW3c} = require('pineglade-w3c');
+const parser = require('posthtml-parser');
+const render = require('posthtml-render');
+const Typograf = require('typograf');
+
+const typograf = new Typograf({
+  locale: ['ru', 'en-US'],
+  htmlEntity: {type: 'name'},
+});
 
 module.exports = {
   plugins: [
+    (() => async (tree) => {
+      const [, , page] = tree.options.from.match(/^.*pages(\\+|\/+)(.*)$/);
+
+      return parser(typograf.execute(render(tree)));
+    })(),
     getPosthtmlW3c({
       exit: process.env.NODE_ENV === 'development',
       forceOffline: true,
@@ -9,7 +22,7 @@ module.exports = {
         return filename
           .replace(/^.*pages(\\+|\/+)(.*)\.twig$/, '$2')
           .replace(/\\/g, '/');
-      }
-    })
-  ]
+      },
+    }),
+  ],
 };
